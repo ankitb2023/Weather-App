78795b918ae2a2eefcf73d2575cde315b9cfcda8
AnkitBhujeja <64649185+AnkitBhujeja@users.noreply.github.com>
2021-06-19 21:38:04 +0530

    Update fetchWeather.js

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 require('dotenv').config();
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-console.log(process.env);
-// const API_KEY = 'f645e9b392ffea26bcdc2733a158d3b0';
-console.log(process.env);
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
         params: {
